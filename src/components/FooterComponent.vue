ffc0dc5f042cd4f00ab0986c5a94d4ba2f1eed5e
DivineCreations <divinecreations1199@gmail.com>
2025-05-28 16:42:12 +0200

    // footerComponent edited

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-blue-800 text-white shadow-md px-6 py-4 rounded-b-2xl rounded-t-2xl">
+  <footer class="bg-blue-800 text-white shadow-md px-6 py-4">
     <div class="container mx-auto text-center">
       <p>© 2025 mypartner - Todos los derechos reservados</p>
     </div>
